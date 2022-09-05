0f923774690d0938f21cfaf2ab4d86b5e353a814
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-09-05 03:03:58 -0500

    fix: stylua install for arm64

--- lua/mason-registry/stylua/init.lua
+++ lua/mason-registry/stylua/init.lua
@@ -2,6 +2,7 @@ local Pkg = require "mason-core.package"
 local _ = require "mason-core.functional"
 local platform = require "mason-core.platform"
 local github = require "mason-core.managers.github"
+local cargo = require "mason-core.managers.cargo"
 
 local coalesce, when = _.coalesce, _.when
 
@@ -14,17 +15,20 @@ return Pkg.new {
     ---@async
     ---@param ctx InstallContext
     install = function(ctx)
-        github
-            .unzip_release_file({
-                repo = "johnnymorganz/stylua",
-                asset_file = coalesce(
-                    when(platform.is.mac_arm64, "stylua-macos-aarch64.zip"),
-                    when(platform.is.mac_x64, "stylua-macos.zip"),
-                    when(platform.is.linux_x64, "stylua-linux.zip"),
-                    when(platform.is.win_x64, "stylua-win64.zip")
-                ),
-            })
-            .with_receipt()
+        if platform.is.mac_arm64 then
+            cargo.crate("stylua", { bin = { "stylua" } })
+        else
+            github
+                .unzip_release_file({
+                    repo = "johnnymorganz/stylua",
+                    asset_file = coalesce(
+                        when(platform.is.mac_x64, "stylua-macos.zip"),
+                        when(platform.is.linux_x64, "stylua-linux.zip"),
+                        when(platform.is.win_x64, "stylua-win64.zip")
+                    ),
+                })
+                .with_receipt()
+        end
         ctx:link_bin("stylua", platform.is.win and "stylua.exe" or "stylua")
     end,
 }
